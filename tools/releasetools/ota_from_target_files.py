c635f4605298d703c2f7ddf6abc3210e6aa92f30
Wolfgang Wiedmeyer <wolfgit@wiedmeyer.de>
2017-02-14 19:01:30 +0100

    rename release key dir to replicant
    Signed-off-by: Wolfgang Wiedmeyer <wolfgit@wiedmeyer.de>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -745,7 +745,7 @@ endif;
   common.ZipWriteStr(output_zip, "system/build.prop",
                      ""+input_zip.read("SYSTEM/build.prop"))
 
-  common.ZipWriteStr(output_zip, "META-INF/org/lineageos/releasekey",
+  common.ZipWriteStr(output_zip, "META-INF/org/replicant/releasekey",
                      ""+input_zip.read("META/releasekey.txt"))
 
 def WritePolicyConfig(file_name, output_zip):
