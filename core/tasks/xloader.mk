615c221149e9bb27b06091843736aa4082c93ffb
Wolfgang Wiedmeyer <wolfgit@wiedmeyer.de>
2017-06-05 22:03:56 +0200

    U-Boot and Xloader: use arm-none-eabi toolchain
    Signed-off-by: Wolfgang Wiedmeyer <wolfgit@wiedmeyer.de>

--- core/tasks/xloader.mk
+++ core/tasks/xloader.mk
@@ -41,7 +41,7 @@ ifeq ($(TARGET_ARCH),arm)
             ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
         endif
     else
-        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) arm-none-eabi-"
     endif
     ccache = 
 endif
